d7e455effa61bdeb1968809727531c2e353866fc
M1chal05 <mjanecek03@seznam.cz>
2023-03-05 14:17:54 +0100

    upload

--- popup.js
+++ popup.js
@@ -3,11 +3,18 @@
 var popupBtn = document.getElementById("popupBtn")
 var popup = document.querySelector(".popup")
 var popupCloseBtn = document.querySelector(".close-popup-btn")
+var header = document.querySelector("header")
+var main = document.querySelector("main")
 
 popupBtn.addEventListener("click", function(){
     popup.style.display = "flex"
+    header.style.display = "none"
+    main.style.display = "none"
 })
 
 popupCloseBtn.addEventListener("click", function(){
     popup.style.display = "none"
+    header.style.display = "flex"
+    main.style.display = "initial"
 })
+
--- script.js
+++ script.js
@@ -122,3 +122,4 @@ slider.addEventListener("scroll", function(){
     }
 })
 
+
